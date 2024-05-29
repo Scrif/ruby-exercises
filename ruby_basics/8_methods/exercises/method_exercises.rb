2455fcb8956608b5f4f62f69a85e18603a5e796e
scrif <39807414+Scrif@users.noreply.github.com>
2024-05-29 18:00:29 +0000

    Started method exercises

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,22 +7,35 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(number)
+    number.chr
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
 
+def common_sports(current_sports, favorite_sports)
+    current_sports.intersection(favorite_sports)
+end
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
 
+def alphabetical_list(games)
+    games.sort.uniq
+end
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+
+def lucky_number(number = 7)
+    "Today's lucky number is #{number}"
+end
 
 
 # method name: #ascii_code
@@ -31,6 +44,12 @@
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+def ascii_code(character)
+    unless character.length = 1
+        return character.ord
+    end
+    return "Input Error"
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -40,6 +59,7 @@
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
 
+def pet_pun(animal)
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
